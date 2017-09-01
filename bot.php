bcb660ccbf89dabb11c54b768e16df2d24cd7b53
Piyapong Thanawang <thanawang3rd@gmail.com>
2017-09-01 17:39:05 +0700

    rollback

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 ini_set('always_populate_raw_post_data', '-1');
 $proxy = 'http://fixie:rnOjMCQkPajq457@velodrome.usefixie.com:80';
 $proxyauth = 'http://fixie:rnOjMCQkPajq457@velodrome.usefixie.com:80';
-$access_token = 'http://fixie:rnOjMCQkPajq457@velodrome.usefixie.com:80';
+$access_token = '+wkE0LfTqTxn8HRBs+HnkoM2FOHGmZhpbVl+Vm13Y0ZsW1Yplfj8UXibnizUAZYABL49/J9Q0AXoNMyurJI8xb1B3lbdEfzaFFU8fZAQ4jVvPLT5m9VNNm1pG3WsjCGHgn5nHb35NHH1vY3FxFr3OQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
